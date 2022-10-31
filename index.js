e7f888cbc995d122062dfed029b4fe2d4004244d
tjsdk1260 <115769429+tjsdk1260@users.noreply.github.com>
2022-10-31 14:51:20 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function openNav(){
     $('#nav-bg-round').toggleClass('open');
 };
 
+
+
 let imgIdx = 0;
 function prev(){
     if(imgIdx == 0){
@@ -22,6 +24,50 @@ function next(){
     }
     $('.bestseller-img ul').css('left',-(100 * imgIdx)+'vh');
 };
+
+function giveFix() {
+    $('.bestseller-img').css({
+        "position": "fixed",
+        "top": 0,
+        "left": 0
+    });
+    $('.bestseller-list').css({
+        "left": "50%"
+    });
+}
+
+function removeFix() {
+    $('.bestseller-img').css({
+        "position": "relative"
+    });
+    $('.bestseller-list').css({
+        "left": "auto"
+    });
+}
+
+function addImgDown(value) {
+    $('.bestseller-img').css({
+        "top": value + "px"
+    });
+}
+
+
+$(window).scroll(function(){
+    const imgHeight = window.innerHeight;
+    const listHeight = $('.bestseller-list').height();
+    console.log(imgHeight, listHeight);
+
+    let scrolls = $(this).scrollTop();
+    if(imgHeight <= scrolls && scrolls <= listHeight){
+        giveFix();
+    }else if(listHeight <= scrolls && scrolls <= imgHeight+listHeight) {
+        removeFix();
+        addImgDown(listHeight - imgHeight);
+    }
+    else{
+        removeFix();
+    }
+});
 
 window.onscroll=()=>{
     if(window.scrollY>=1000){
